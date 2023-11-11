346e88cb764c4bf6fa22382ef3591cfb179c82fa
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-11 15:17:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function fibonacci(num) {
 // your code here
+	let fib =[0, 1];
+	for(let i = 2; i < num; i++){
+		fib[i] = fib[i-1] + fib[i-2];
+	}
+	return fib[n-1];
 }
 
 module.exports = fibonacci;
